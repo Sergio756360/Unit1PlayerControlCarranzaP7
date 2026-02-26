1280f423d45b1fdc315e527eb427717221495a44
Sergio756360 <756360@student.dusd.net>
2026-02-26 15:25:31 -0800

    New Scripts
    Added a new script to move the propellors for the Plane

--- P7PlayerControlCarranza/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ P7PlayerControlCarranza/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -7,6 +7,7 @@ public class PlayerControllerX : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    private Vector3 offset = new Vector3(30, 0, 10);
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +22,9 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * + Time.deltaTime);
     }
 }
---
+++ P7PlayerControlCarranza/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    private float propellorSpeed = 1000;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, propellorSpeed * Time.deltaTime);
+    }
+}
